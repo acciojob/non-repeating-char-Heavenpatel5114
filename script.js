9965b9eaf06a20587e5fc3cb06aee356a0b0426d
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-04-18 09:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const string = "heeaven";
+let index = -1;
+let fnc = ' ';
+for (let i of string) {
+    if (string.split(i).length - 1 === 1) {
+        fnc = i;
+        break;
+    } else {
+        index += 1;
+    }
+}
+if (index === 1) {
+    console.log("Either all characters are repeating or string is empty");
+} else {
+    console.log(`First non-repeating character is ${fnc}`);
+}
